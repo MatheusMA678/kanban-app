21f8e397e5aee8ef468d599c97ee0310028c7a05
Matheus Mattos Avelino <114790698+MatheusMA678@users.noreply.github.com>
2023-03-25 16:12:13 -0400

    update

--- src/components/Home/KanbanLists.tsx
+++ src/components/Home/KanbanLists.tsx
@@ -25,15 +25,42 @@ const cardContainerVariants = {
   },
 };
 
+const cards = [
+  {
+    id: 1,
+    title: "Título",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    tags: ["rocketseat"],
+  },
+  {
+    id: 2,
+    title: "Título",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    tags: ["rocketseat"],
+  },
+  {
+    id: 3,
+    title: "Título",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    tags: ["rocketseat"],
+  },
+  {
+    id: 4,
+    title: "Título",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    tags: ["rocketseat"],
+  },
+];
+
 export const KanbanLists = () => {
   return (
     <section className="grid grid-cols-3 gap-12">
       <motion.div className="flex flex-col gap-8">
         <h2 className="text-xl font-bold">A fazer</h2>
         <motion.div className="flex flex-col gap-4">
-          {[...Array(5).keys()].map((i) => (
+          {cards.map((card, i) => (
             <motion.div
-              key={i}
+              key={card.id}
               initial={{
                 opacity: 0,
                 y: 20,
@@ -47,22 +74,22 @@ export const KanbanLists = () => {
                 delay: i * 0.5,
               }}
               whileHover={{
-                scale: 1.05,
                 cursor: "pointer",
-                transition: {
-                  duration: 0.2,
-                  delay: 0,
-                },
               }}
               className="w-full h-40 rounded-lg bg-white shadow-lg p-6 flex flex-col justify-between"
             >
-              <h3 className="font-bold text-sm">Título</h3>
+              <h3 className="font-bold text-sm">{card.title}</h3>
               <p className="text-sm text-gray-600 justify-self-start overflow-hidden">
-                Lorem ipsum dolor sit amet consectetur adipisicing.
+                {card.desc}
               </p>
-              <span className="bg-primary/20 font-bold text-xs w-fit rounded-lg px-2 py-1 text-primary">
-                Rocketseat
-              </span>
+              {card.tags.map((tag) => (
+                <span
+                  key={tag}
+                  className="bg-primary/20 font-bold text-xs w-fit rounded-lg px-2 py-1 text-primary"
+                >
+                  {tag}
+                </span>
+              ))}
             </motion.div>
           ))}
         </motion.div>
